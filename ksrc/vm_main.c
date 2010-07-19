5979db0255bbe3ab4ff6370dc2b04bb78d225726
Alexey Lyashkov <alexey.lyashkov@clusterstor.com>
2010-07-19 10:31:39 +0300

    attach main loop to build

--- ksrc/vm_main.c
+++ ksrc/vm_main.c
@@ -1,10 +1,11 @@
-#include <errno.h>
-#include <stdint.h>
-#include "list.h"
+#include <linux/slab.h>
+#include <linux/vmalloc.h>
+#include <asm/uaccess.h>
+#include <linux/errno.h>
 
 #include "fifo.h"
-
-#include "vm_defs.h"
+#include "../include/vm_defs.h"
+#include "vm_api.h"
 #include "vm_core.h"
 
 int vm_interpret_init(struct stack_vm **vm, int stack_size,
@@ -13,18 +14,28 @@ int vm_interpret_init(struct stack_vm **vm, int stack_size,
 	struct fifo *stack;
 	char *program;
 	struct stack_vm *v;
+	int rc;
 
-	v = malloc(sizeof *v);
+	v = kmalloc(sizeof *v, GFP_KERNEL);
 	if (v == NULL)
 		return -ENOMEM;
 
 	stack = fifo_create(stack_size);
-	if (stack == NULL)
+	if (stack == NULL) {
+		rc = -ENOMEM;
 		goto err1;
+	}
 
-	program = malloc(size);
-	if (program == NULL) 
+	program = vmalloc(size);
+	if (program == NULL) {
+		rc = -ENOMEM;
 		goto err2;
+	}
+
+	if (copy_from_user(program, prg, size)) {
+		rc = -EFAULT;
+		goto err3;
+	}
 
 	v->sv_ip = 0;
 	v->sv_program = program;
@@ -34,65 +45,68 @@ int vm_interpret_init(struct stack_vm **vm, int stack_size,
 
 	*vm = v;
 	return 0;
+err3:
+	vfree(program);
 err2:
 	fifo_destroy(stack);
 err1:
-	free(v);
-	return -ENOMEM;
+	kfree(v);
+	return rc;
 }
 
 void vm_interpret_fini(struct stack_vm *vm)
 {
-	free(vm->sv_program);
-	fifo_destroy(sv->stack);
-	free(vm);
+	vfree(vm->sv_program);
+	fifo_destroy(vm->sv_stack);
+	kfree(vm);
 }
 
 int vm_interpret_run(struct stack_vm *vm)
 {
 	char op;
-	int rc;
+	int rc = -ENODATA;
 	long old_ip;
 
+	vm->sv_run = 1;
 	while(vm->sv_ip < vm->sv_size) {
 		old_ip = vm->sv_ip;
-		op = vm_program[vm->sv_ip ++];
+		op = vm->sv_program[vm->sv_ip ++];
 		switch (op) {
 		case VM_CMD_PUSHS:
-			rc = vm_pushs(vm, vm_program[vm->sv_ip]);
+			rc = vm_pushs(vm, &vm->sv_program[vm->sv_ip]);
 			break;
 		case VM_CMD_PUSHL:
-			rc = vm_pushl(vm, vm_program[vm->sv_ip]);
+			rc = vm_pushl(vm, &vm->sv_program[vm->sv_ip]);
 			break;
 		case VM_CMD_CMPS:
-			rc = vm_cmps(vm, vm_program[vm->sv_ip]);
+			rc = vm_cmps(vm, &vm->sv_program[vm->sv_ip]);
 			break;
 		case VM_CMD_CMPL:
-			rc = vm_cmpl(vm, vm_program[vm->sv_ip]);
+			rc = vm_cmpl(vm, &vm->sv_program[vm->sv_ip]);
 			break;
 		case VM_CMD_CALL:
-			rc = vm_call(vm, vm_program[vm->sv_ip]);
+			rc = vm_call(vm, &vm->sv_program[vm->sv_ip]);
 			break;
 		case VM_CMD_GOTO:
-			rc = vm_goto(vm, vm_program[vm->sv_ip]);
+			rc = vm_goto(vm, &vm->sv_program[vm->sv_ip]);
 			break;
 		case VM_CMD_JZ:
-			rc = vm_jz(vm, vm_program[vm->sv_ip]);
+			rc = vm_jz(vm, &vm->sv_program[vm->sv_ip]);
 			break;
 		case VM_CMD_ADD:
-			rc = vm_add(vm, vm_program[vm->sv_ip]);
+			rc = vm_add(vm, &vm->sv_program[vm->sv_ip]);
 			break;
 		case VM_CMD_SUB:
-			rc = vm_sub(vm, vm_program[vm->sv_ip]);
+			rc = vm_sub(vm, &vm->sv_program[vm->sv_ip]);
 			break;
 		case VM_CMD_DUP:
-			rc = vm_dup(vm, vm_program[vm->sv_ip]);
-			break
+			rc = vm_dup(vm, &vm->sv_program[vm->sv_ip]);
+			break;
 		case VM_CMD_NOP:
-			rc = vm_nop(vm, vm_program[vm->sv_ip]);
-			break
+			rc = vm_nop(vm, &vm->sv_program[vm->sv_ip]);
+			break;
 		default:
-			printk(KERN_ERR, "unknow op (%c) !\n", op);
+			printk(KERN_ERR "unknow op (%c) !\n", op);
 			rc = -EINVAL;
 			break;
 		};
@@ -108,6 +122,8 @@ int vm_interpret_run(struct stack_vm *vm)
 		if (rc == 0)
 			rc = exit_rc;
 	}
+	vm->sv_run = 0;
+	vm->sv_rc = rc;
 	return rc;
 }
 
