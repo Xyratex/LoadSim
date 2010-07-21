a7f3d7eadefef3472cbf0c67591c08fbd03069be
Alexey Lyashkov <alexey.lyashkov@clusterstor.com>
2010-07-21 12:18:33 +0300

    code cleanup

--- ksrc/fifo.c
+++ ksrc/fifo.c
@@ -5,9 +5,7 @@
 
 struct fifo {
 	int ff_size;
-	int ff_used;
 	int ff_top;
-	int ff_bottom;
 	long ff_data[0];
 };
 
@@ -21,10 +19,8 @@ struct fifo *fifo_create(int size)
 		return NULL;
 	}
 
-	f->ff_size = size;
+	f->ff_size = size - 1;
 	f->ff_top = 0;
-	f->ff_bottom = 0;
-	f->ff_used = 0;
 
 	return f;
 }
@@ -34,7 +30,7 @@ struct fifo *fifo_create(int size)
  */
 void fifo_destroy(struct fifo *fifo)
 {
-	if (fifo->ff_used) {
+	if (fifo->ff_top != 0) {
 		err_print("leak\n");
 	}
 
@@ -44,28 +40,26 @@ void fifo_destroy(struct fifo *fifo)
 
 int fifo_push(struct fifo *f, long data)
 {
-	if (f->ff_used == f->ff_size) {
+	if (f->ff_top == f->ff_size) {
 		err_print("not free space in fifo for push\n");
 		return -ENOMEM;
 	}
 
 	f->ff_data[f->ff_top] = data;
-	f->ff_top = (f->ff_top + 1) % f->ff_size;
-	f->ff_used ++;
+	f->ff_top ++;
 
 	return 0;
 }
 
 int fifo_pop(struct fifo *f, long *data)
 {
-	if (f->ff_used == 0) {
+	if (f->ff_top == 0) {
 		err_print("fifo empty for pop\n");
 		return -ENODATA;
 	}
 
-	*data = f->ff_data[f->ff_bottom];
-	f->ff_bottom = (f->ff_bottom + 1) % f->ff_size;
-	f->ff_used --;
+	f->ff_top --;
+	*data = f->ff_data[f->ff_top];
 
 	return 0;
 }
